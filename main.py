d852f1450c3e563c25f44a5cff6f1704e632f0b0
kkaterinasoroka <77160097+kkaterinasoroka@users.noreply.github.com>
2021-01-10 11:09:28 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ item2 = randint(0,10)
 item3= item1 + item2
 basic.show_number(item1)
 basic.show_number(item2)
-if item3 % 2:
-   basic.show_string("press A") 
+if item3 % 0:
+   basic.show_string("if sum is even press 'A'") 
+else:
+    basic.show_string("if sum is odd press 'B'")
+NExt I will use input and let the user press a or b
--- main.ts
+++ main.ts
@@ -4,7 +4,9 @@ let item2 = randint(0, 10)
 let item3 = item1 + item2
 basic.showNumber(item1)
 basic.showNumber(item2)
-if (item3 % 2) {
-    basic.showString("press A")
+if (item3 % 0) {
+    basic.showString("if sum is even press 'A'")
+} else {
+    basic.showString("if sum is odd press 'B'")
 }
 
