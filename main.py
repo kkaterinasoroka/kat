668ee0b0ad49382174ead3d8cd5d3fafdb3f5c3c
kkaterinasoroka <77160097+kkaterinasoroka@users.noreply.github.com>
2021-01-10 11:06:51 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,15 +4,5 @@ item2 = randint(0,10)
 item3= item1 + item2
 basic.show_number(item1)
 basic.show_number(item2)
-
-basic.show_string("add the next two numbers")
-basic.show_number(16)
-basic.show_number(23)
-basic.show_string("if #: 28 press 'A'")
-basic.show_string("if #: 29 press 'B'")
-def on_button_pressed_a():
-    basic.show_string("incorrect:(")
-input.on_button_pressed(Button.A, on_button_pressed_a)
-def on_button_pressed_b():
-    basic.show_string("correct!")
-input.on_button_pressed(Button.B, on_button_pressed_b)
+if item3 % 2:
+   basic.show_string("press A") 
--- main.ts
+++ main.ts
@@ -4,14 +4,7 @@ let item2 = randint(0, 10)
 let item3 = item1 + item2
 basic.showNumber(item1)
 basic.showNumber(item2)
-basic.showString("add the next two numbers")
-basic.showNumber(16)
-basic.showNumber(23)
-basic.showString("if #: 28 press 'A'")
-basic.showString("if #: 29 press 'B'")
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    basic.showString("incorrect:(")
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showString("correct!")
-})
+if (item3 % 2) {
+    basic.showString("press A")
+}
+
